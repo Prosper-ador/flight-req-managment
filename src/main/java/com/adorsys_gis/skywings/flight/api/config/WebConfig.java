fff5daa47220eaa16ce026b5dd3488d43b56ad51
prosper-ador <prosnyengka@gmail.com>
2025-07-10 09:13:53 +0100

    feat: add @JsonIgnore annotation to tickets list in Booking entity
    - Added @JsonIgnore to the tickets field to prevent serialization of ticket data when converting Booking objects to JSON.
    - Enhances data privacy and reduces payload size in API responses.

---
+++ src/main/java/com/adorsys_gis/skywings/flight/api/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.adorsys_gis.skywings.flight.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Value("${cors.allowed-origins}")
+            private String[] allowedOrigins;
+                    
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(allowedOrigins)
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
